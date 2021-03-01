6d0c4c5b730a59ac2faf194470dbad5c79007bc2
Alexis Montoison <alexis.montoison@polymtl.ca>
2021-02-28 21:03:55 -0500

    Julia interface to qr_mumps 3.0.1

--- src/qr_mumps.jl
+++ src/qr_mumps.jl
@@ -1,170 +1,374 @@
 module qr_mumps
 
-export QrmType,
-       qrm_analyse, qrm_analyze, qrm_factorize,
-       qrm_apply, qrm_solve, qrm_solve!,
-       qrm_least_squares, qrm_least_squares!,
-       qrm_pseti
+using CEnum, Libdl, SparseArrays, LinearAlgebra
 
-if isfile(joinpath(dirname(@__FILE__), "..", "deps", "deps.jl"))
-  include("../deps/deps.jl")
+import Base: \, size, length
+
+export \
+    
+if haskey(ENV, "JULIA_QR_MUMPS_LIBRARY_PATH")
+  println("Custom Installation")
+  const libsqrm = joinpath(ENV["JULIA_QR_MUMPS_LIBRARY_PATH"], "libsqrm.$dlext")
+  const libdqrm = joinpath(ENV["JULIA_QR_MUMPS_LIBRARY_PATH"], "libdqrm.$dlext")
+  const libcqrm = joinpath(ENV["JULIA_QR_MUMPS_LIBRARY_PATH"], "libcqrm.$dlext")
+  const libzqrm = joinpath(ENV["JULIA_QR_MUMPS_LIBRARY_PATH"], "libzqrm.$dlext")
+  const libqrm_common = joinpath(ENV["JULIA_QR_MUMPS_LIBRARY_PATH"], "libqrm_common.$dlext")
 else
-  error("QR_MUMPS library not properly installed. Please run Pkg.build(\"qr_mumps\")")
+  println("Yggdrasil Installation")
+  using qr_mumps_jll
 end
 
-"Exception type raised in case of error."
-type QRMException <: Exception
-  msg :: ASCIIString
-end
+include("wrapper/qr_mumps_common.jl")
+include("wrapper/qr_mumps_api.jl")
 
+export qrm_spmat, qrm_spfct,
+    qrm_init, qrm_finalize,
+    qrm_spmat_init!, qrm_spmat_init, qrm_spmat_destroy!,
+    qrm_spfct_init!, qrm_spfct_init, qrm_spfct_destroy!,
+    qrm_readmat!, qrm_readmat,
+    qrm_analyse!, qrm_analyse,
+    qrm_factorize!,
+    qrm_solve!, qrm_solve,
+    qrm_apply!, qrm_apply,
+    qrm_spmat_mv!, qrm_spmat_nrm, qrm_vecnrm,
+    qrm_spbackslash!, qrm_spbackslash,
+    qrm_spposv!, qrm_spposv,
+    qrm_least_squares!, qrm_least_squares,
+    qrm_min_norm!, qrm_min_norm,
+    qrm_residual_norm, qrm_residual_orth,
+    qrm_spfct_seti, qrm_spfct_geti, qrm_spfct_getii,
+    qrm_gseti, qrm_ggeti, qrm_ggetii
 
-type QrmType_Private
-  irn :: Ptr{Cint}
-  jcn :: Ptr{Cint}
-  val :: Ptr{Cdouble}
-  m   :: Cint
-  n   :: Cint
-  nz  :: Cint
-  cperm_in :: Ptr{Cint}
-  icntl :: Ptr{Cint}
-  rcntl :: Ptr{Cdouble}
-  gstats :: Ptr{Clong}
-  h   :: Cint
+@doc raw"""
+    qrm_init(ncpu, ngpu)
 
-  function QrmType_Private(irn :: Vector{Int}, jcn :: Vector{Int}, val :: Vector{Float64},
-                           m :: Int, n :: Int, nz :: Int)
-    qrm_priv = new(C_NULL, C_NULL, C_NULL, 0, 0, 0, C_NULL, C_NULL, C_NULL, C_NULL, 0)
-    ccall((:dqrm_spmat_init_c, libdqrm), Void, (Ptr{QrmType_Private},), &qrm_priv)
-    qrm_priv.irn = pointer(irn)
-    qrm_priv.jcn = pointer(jcn)
-    qrm_priv.val = pointer(val)
-    qrm_priv.m = m
-    qrm_priv.n = n
-    qrm_priv.nz = nz
-    # cperm_in can remain NULL for now.
-    # icntl, rcntl and gstats are allocated inside QR_MUMPS.
-    finalizer(qrm_priv, qrm_private_finalize)
-    return qrm_priv
-  end
-end
+This routine initializes qr\_mumps and should be called prior to any other qr\_mumps routine.
 
-type QrmType
+#### Input Arguments :
 
-  __qrm :: QrmType_Private
-  irn :: Vector{Cint}  # to ensure references persist.
-  jcn :: Vector{Cint}
-  val :: Vector{Cdouble}
+* ncpu: number of working threads on CPU.
+* ngpu: number of working threads on GPU.
+"""
+function qrm_init end
 
-  function QrmType(irn :: Vector{Int}, jcn :: Vector{Int}, val :: Vector{Float64},
-                   m :: Int, n :: Int, nz :: Int)
+@doc raw"""
+    qrm_finalize()
 
-    qrm_priv = QrmType_Private(irn, jcn, val, m, n, nz)
-    qrm = new(qrm_priv, irn, jcn, val)
-    finalizer(qrm, qrm_finalize)
-    return qrm
-  end
-end
+This routine finalizes qr\_mumps and no other qr\_mumps routine should be called afterwards.
+"""
+function qrm_finalize end
 
+@doc raw"""
+    qrm_spmat_init!(spmat, A; sym=false)
 
-# Constructor.
-function QrmType(A :: SparseMatrixCSC{Float64,Int})
-  m, n = size(A)
-  nz = nnz(A)
-  irn, jcn, val = findnz(A)
-  return QrmType(irn, jcn, val, m, n, nz)
-end
+This routine initializes a **qrm_spmat** type data structure from a **sparseMatrixCSC**.
 
-QrmType(A :: Array{Float64,2}) = QrmType(sparse(A))
+#### Input Arguments :
 
+* spmat: the **qrm_spmat** sparse matrix to be initialized.
+* A : a Julia sparse matrix stored in **sparseMatrixCSC** format.
+* sym : a boolean to specify if the matrix is symmetric (true) or unsymmetric (false).
+"""
+function qrm_spmat_init! end
 
-# Destructors.
-function qrm_private_finalize(qrm_priv :: QrmType_Private)
-  # Don't print in the finalizer, or else:
-  # jl_uv_writecb() ERROR: bad file descriptor EBADF
-  ccall((:dqrm_spmat_destroy_c, libdqrm), Void, (Ptr{QrmType_Private},), &qrm_priv)
-  qrm_priv.irn = C_NULL
-  qrm_priv.jcn = C_NULL
-  qrm_priv.val = C_NULL
-  qrm_priv.cperm_in = C_NULL
-  qrm_priv.icntl = C_NULL
-  qrm_priv.rcntl = C_NULL
-  qrm_priv.gstats = C_NULL
-end
+@doc raw"""
+    spmat = qrm_spmat_init(A; sym=false)
+"""
+function qrm_spmat_init end
 
-function qrm_finalize(qrm :: QrmType)
-  qrm_private_finalize(qrm.__qrm)
-end
+@doc raw"""
+    qrm_spmat_destroy!(spmat)
 
+This routine cleans up a **qrm_spmat** type data structure.
 
-# import Base.show, Base.print
-# function show(io :: IO, qrm :: QrmType)
-#   print(io, "QRM: m=$(qrm.m), n=$(qrm.n), nz=$(qrm.nz)");
-# end
-#
-# function print(io :: IO, qrm :: QrmType)
-#   print(io, "QRM: m=$(qrm.m), n=$(qrm.n), nz=$(qrm.nz)");
-# end
+#### Input Argument :
 
+* spfct: the sparse matrix to be destroyed.
+"""
+function qrm_spmat_destroy! end
 
-function qrm_pseti(qrm :: QrmType, param :: ASCIIString, val :: Int)
-  ccall((:dqrm_pseti_c, libdqrm), Void, (Ptr{QrmType}, Ptr{Uint8}, Cint), &qrm, param, val)
-end
+@doc raw"""
+    qrm_spfct_init!(spmat, spfct)
 
+This routine initializes a **qrm_spfct** type data structure.
+This amounts to setting all the control parameters to the default values.
 
-function qrm_analyse(qrm :: QrmType; transp :: Char='n')
-  ccall((:dqrm_analyse_c, libdqrm), Void, (Ptr{QrmType}, Cchar), &qrm, transp)
-end
+#### Input Arguments :
 
-# Zed's not dead!
-qrm_analyze = qrm_analyse
+* spmat: the input matrix of qrm_spmat type.
+* spfct: the sparse factorization object to be initialized.
+"""
+function qrm_spfct_init! end
 
+@doc raw"""
+    spfct = qrm_spfct_init(spmat)
+"""
+function qrm_spfct_init end
 
-function qrm_factorize(qrm :: QrmType; transp :: Char='n')
-  ccall((:dqrm_factorize_c, libdqrm), Void, (Ptr{QrmType}, Cchar), &qrm, transp)
-end
+@doc raw"""
+    qrm_spfct_destroy!(spfct)
 
+This routine cleans up a **qrm_spfct** type data structure by deleting the result of a sparse factorization.
 
-function qrm_apply(qrm :: QrmType, b :: Vector{Float64}; transp :: Char='t')
-  # transp = 't' means we apply Q' to b.
-  # This is consistent with transp = 'n' in the analysis and factorization.
-  # It's qr_mumps that is inconsistent.
-  ccall((:dqrm_apply_c, libdqrm), Void,
-        (Ptr{QrmType}, Cchar,  Ptr{Cdouble}, Cint),
-         &qrm,         transp, b,            1)
-end
+#### Input Argument :
 
+* spfct: the sparse factorization object to be destroyed.
+"""
+function qrm_spfct_destroy! end
 
-function qrm_solve(qrm :: QrmType, b :: Vector{Float64}; transp :: Char='n')
-  x = zeros(Cdouble, transp == 'n' ? qrm.n : qrm.m)
-  qrm_solve!(qrm, b, x, transp=transp)
-  return x
-end
+@doc raw"""
+    qrm_analyse!(spmat, spfct; transp='n')
 
-function qrm_solve!(qrm :: QrmType, b :: Vector{Float64}, x :: Vector{Float64}; transp :: Char='n')
-  length(x) ≥ (transp == 'n' ? qrm.n : qrm.m) || error("qr_mumps: size of x insufficient")
-  ccall((:dqrm_solve_c, libdqrm), Void,
-        (Ptr{QrmType}, Cchar,  Ptr{Cdouble}, Ptr{Cdouble}, Cint),
-         &qrm,         transp, b,            x,            1)
-end
+This routine performs the analysis phase and updates spfct.
 
+#### Input Arguments :
 
-function qrm_least_squares(qrm :: QrmType, b :: Vector{Float64})
-  x = zeros(Cdouble, qrm.n)
-  qrm_least_squares!(qrm, b, x)
-  return x
-end
+* spmat: the input matrix of qrm_spmat type.
+* spfct: the sparse factorization object of qrm_spfct type.
+* transp: whether the input matrix should be transposed or not. Can be either 't', 'c' or 'n'.
+"""
+function qrm_analyse! end
 
-function qrm_least_squares!(qrm :: QrmType, b :: Vector{Float64}, x :: Vector{Float64})
-  length(x) ≥ qrm.n || error("qr_mumps: size of x insufficient")
-  ccall((:dqrm_least_squares_c, libdqrm), Void,
-        (Ptr{QrmType}, Ptr{Cdouble}, Ptr{Cdouble}, Cint),
-         &qrm,         b,            x,            1)
-end
+@doc raw"""
+    spfct = qrm_analyse(spmat; transp='n')
+"""
+function qrm_analyse end
 
-function qrm_least_squares(A :: SparseMatrixCSC{Float64,Int}, b :: Vector{Float64})
-  qrm = QrmType(A)
-  return qrm_least_squares(qrm, b)
-end
+@doc raw"""
+    qrm_factorize!(spmat, spfct; transp'n')
 
+This routine performs the factorization phase. It can only be executed if the analysis is already done.
+
+#### Input Arguments :
+
+* spmat: the input matrix of qrm_spmat type.
+* spfct: the sparse factorization object of qrm_spfct type.
+* transp: whether the input matrix should be transposed or not. Can be either 't', 'c' or 'n'.
+"""
+function qrm_factorize! end
+
+@doc raw"""
+    qrm_solve!(spfct, b, x; transp='n')
+
+This routine solves the triangular system `Rx = b` or `Rᵀx = b`. It can only be executed once the factorization is done.
+
+#### Input Arguments :
+
+* spfct: the sparse factorization object resulting from the qrm_factorize! function.
+* b: the right-hand side(s).
+* x: the solution vector(s).
+* transp: whether to solve for R or Rᵀ. Can be either 't', 'c' or 'n'.
+"""
+function qrm_solve! end
+
+@doc raw"""
+    x = qrm_solve(spfct, b; transp='n')
+"""
+function qrm_solve end
+
+@doc raw"""
+    qrm_apply!(spfct, b; transp='n')
+
+This routine computes `z = Qb` or `z = Qᵀb` in place and overwrites b. It can only be executed once the factorization is done.
+
+#### Input Arguments :
+
+* spfct: the sparse factorization object resulting from the qrm_factorize! function.
+* b: the vector(s) to which Q or Qᵀ is applied.
+* transp: whether to apply Q or Qᵀ. Can be either 't', 'c' or 'n'.
+"""
+function qrm_apply! end
+
+@doc raw"""
+    z = qrm_apply(spfct, b; transp='n')
+"""
+function qrm_apply end
+
+@doc raw"""
+    qrm_spmat_mv!(spmat, alpha, x, beta, y; transp='n')
+
+This subroutine performs a matrix-vector product of the type y = αAx + βy or y = αAᵀx + βy.
+
+#### Input Arguments :
+
+* spmat: the input matrix.
+* alpha, beta : the α and β scalars
+* x: the x vector(s).
+* y: the y vector(s).
+* transp: whether to multiply by A or Aᵀ. Can be either 't', 'c' or 'n'.
+"""
+function qrm_spmat_mv! end
+
+@doc raw"""
+    qrm_spmat_nrm(spmat, ntype, nrm)
+
+This routine computes the one-norm ``\|A\|_1``, the infinity-norm ``\|x\|_{\infty}`` or the two-norm ``\|x\|_2`` of a matrix.
+
+#### Input Arguments :
+
+* spmat: the input matrix.
+* ntype: the type of norm to be computed. It can be either 'i', '1' or 'f' for the infinity, one and Frobenius norms, respectively.
+* nrm: the computed norm.
+"""
+function qrm_spmat_nrm end
+
+@doc raw"""
+    qrm_vecnrm(x, ntype, nrm)
+
+This routine computes the one-norm ``\|x\|_1``, the infinity-norm ``\|x\|_{\infty}`` or the two-norm ``\|x\|_2`` of a vector.
+
+#### Input Arguments :
+
+* x: the x vector(s).
+* ntype: the type of norm to be computed. It can be either 'i', '1' or '2' for the infinity, one and two norms, respectively.
+* nrm the computed norm(s). If x is a matrix this argument has to be a vector and each of its elements will contain the norm of the corresponding column of x.
+"""
+function qrm_vecnrm end
+
+@doc raw"""
+    qrm_spbackslash!(spmat, b, x)
+
+TO DO !
+"""
+function qrm_spbackslash! end
+
+@doc raw"""
+    x = qrm_spbackslash(spmat, b)
+"""
+function qrm_spbackslash end
+
+@doc raw"""
+    qrm_spposv!(spmat, b, x)
+
+This function can be used to solve a linear symmetric, positive definite problem.
+It is a shortcut for the sequence
+
+    x = b
+    qrm_analyse!(spmat, 'n')
+    qrm_factorize!(spmat, 'n')
+    qrm_solve!(spmat, 'c', x, b)
+    qrm_solve!(spmat, 'n', b, x)
+
+#### Input Arguments :
+
+* spmat: the input matrix.
+* b: the right-hand side(s).
+* x: the solution vector(s).
+"""
+function qrm_spposv! end
+
+@doc raw"""
+    x = qrm_spposv(spmat, b)
+"""
+function qrm_spposv end
+
+@doc raw"""
+    qrm_least_squares!(spmat, b, x)
+
+This function can be used to solve a linear least squares problem
+
+```math
+\min \|Ax − b\|_2
+```
+
+in the case where the input matrix is square or overdetermined.
+It is a shortcut for the sequence
+
+    qrm_analyse!(spmat, spfct, 'n')
+    qrm_factorize!(spmat, spfct, 'n')
+    qrm_apply!(spfct, 'c', b)
+    qrm_solve!(spfct, 'n', b, x)
+
+#### Input Arguments :
+
+* spmat: the input matrix.
+* b: the ight-hand side(s).
+* x: the solution vector(s).
+"""
+function qrm_least_squares! end
+
+@doc raw"""
+    x = qrm_least_squares(spmat, b)
+"""
+function qrm_least_squares end
+
+@doc raw"""
+    qrm_min_norm!(spmat, b, x)
+
+This function can be used to solve a linear minimum norm problem
+
+```math
+\min \|x\|_2 \quad s.t. \quad Ax = b
+```
+
+in the case where the input matrix is square or underdetermined.
+It is a shortcut for the sequence
+
+    qrm_analyse!(spmat, 'c')
+    qrm_factorize!(spmat, 'c')
+    qrm_solve!(spmat, 'c', b, x)
+    qrm_apply!(spmat, 'n', x)
+
+#### Input Arguments :
+
+* spmat: the input matrix.
+* b: the right-hand side(s).
+* x: the solution vector(s).
+"""
+function qrm_min_norm! end
+
+"""
+    x = qrm_min_norm(spmat, b)
+"""
+function qrm_min_norm end
+
+@doc raw"""
+    qrm_residual_norm(spmat, b, x)
+
+This function computes the scaled norm of the residual ``\frac{\|b - Ax\|_{\infty}}{\|b\|_{\infty} + \|x\|_{\infty} \|A\|_{\infty}}``, i.e., the normwise backward error.
+
+#### Input Arguments :
+
+* spmat: the input matrix.
+* b: the right-hand side(s).
+* x: the solution vector(s).
+"""
+function qrm_residual_norm end
+
+@doc raw"""
+    qrm_residual_orth(spmat, r)
+
+Computes the quantity ``\frac{\|A^T r\|_2}{\|r\|_2}`` which can be used to evaluate the quality of the solution of a least squares problem.
+
+#### Input Arguments :
+
+* spmat: the input matrix.
+* r: the residual(s).
+"""
+function qrm_residual_orth end
+
+"TO DO!"
+function qrm_readmat! end
+
+"TO DO!"
+function qrm_readmat end
+
+"TO DO!"
+function qrm_spfct_seti end
+
+"TO DO!"
+function qrm_spfct_geti end
+
+"TO DO!"
+function qrm_spfct_getii end
+
+"TO DO!"
+function qrm_gseti end
+
+"TO DO!"
+function qrm_ggeti end
+
+"TO DO!"
+function qrm_ggetii end
 
 end # module
