42e1778028f3cdea4eaf544b3fd85586c16263f3
Alexis Montoison <alexis.montoison@polymtl.ca>
2021-04-15 10:55:38 -0400

    Add documentation about optional features

--- docs/make.jl
+++ docs/make.jl
@@ -10,14 +10,14 @@ makedocs(
   sitename = "qr_mumps.jl",
   pages = ["Introduction" => "index.md",
            "Features" => "features.md",
+           "Optional features" => "optional_features.md",
            "API" => "api.md",
            "Control parameters" => "control_parameters.md",
            "Information parameters" => "information_parameters.md",
            "Performance tuning" => "performance.md",
            "Tutorials" => ["Symmetric and positive definite linear systems" => "tutorials/spd.md",
                            "Least-squares problems" => "tutorials/ls.md",
-                           "Least-norm problems" => "tutorials/ln.md"
-                          ],
+                           "Least-norm problems" => "tutorials/ln.md"],
            "Reference" => "reference.md",
           ]
 )
